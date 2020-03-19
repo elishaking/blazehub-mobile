c206f933753ac781a2f4436ba1ae377a77e5e50c
King Elisha <ek.chibueze@gmail.com>
2020-03-19 15:39:46 +0100

    integrated redux

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
-import 'package:blazehub/pages/landing.dart';
+import 'package:blazehub/models/app.dart';
+import 'package:blazehub/reducers/root.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+import 'package:blazehub/pages/landing.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final store = Store<AppState>(
+    appStateReducer,
+    initialState: AppState.initialState(),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BlazeHub',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+    return StoreProvider(
+      store: store,
+      child: MaterialApp(
+        title: 'BlazeHub',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: Landing(),
       ),
-      home: Landing(),
     );
   }
 }
