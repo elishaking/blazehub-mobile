f41024acb35d840398e70645f237a5809ddadd3e
King Elisha <ek.chibueze@gmail.com>
2020-03-19 13:40:11 +0100

    auth actions

--- lib/actions/auth.dart
+++ lib/actions/auth.dart
@@ -1,1 +1,14 @@
 // todo: implement auth actions
+import 'package:blazehub/models/auth.dart';
+
+class GetErrors {
+  final payload;
+
+  GetErrors(this.payload);
+}
+
+class SetCurrentUser {
+  final AuthUser payload;
+
+  SetCurrentUser(this.payload);
+}
