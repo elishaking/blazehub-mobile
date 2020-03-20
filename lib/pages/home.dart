bea94e6aaf612a65ff0faab360de78d49e83cc23
King Elisha <ek.chibueze@gmail.com>
2020-03-20 14:38:55 +0100

    create post

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,7 +5,16 @@ import 'package:redux/redux.dart';
 import 'package:blazehub/models/app.dart';
 import 'package:blazehub/view_models/home.dart';
 
+class Post {
+  String text;
+
+  Post({this.text});
+}
+
 class Home extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final _post = Post();
+
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, HomeViewModel>(
@@ -15,8 +24,77 @@ class Home extends StatelessWidget {
           appBar: AppBar(
             title: Text('BlazeHub'),
           ),
-          body: Column(
-            children: <Widget>[],
+          body: ListView(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xffe7dff1),
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      decoration: BoxDecoration(
+                        color: Color(0xffe7dff1),
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(10),
+                          bottom: Radius.circular(0),
+                        ),
+                      ),
+                      child: Text(
+                        "Create Post",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            minLines: 4,
+                            maxLines: 7,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.person),
+                              hintText: "Share your thoughts",
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 10),
+                              border: InputBorder.none,
+                            ),
+                            onSaved: (String text) {
+                              _post.text = text;
+                            },
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: ButtonBar(
+                              children: <Widget>[
+                                RaisedButton(
+                                  onPressed: () {
+                                    if (_formKey.currentState.validate()) {
+                                      _formKey.currentState.save();
+                                    }
+                                  },
+                                  child: Text('Post'),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         );
       },
