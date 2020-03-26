9155937064bd3d458915463d9be9ebf44815a909
King Elisha <ek.chibueze@gmail.com>
2020-03-26 10:42:48 +0100

    profile view model

---
+++ lib/view_models/profile.dart
@@ -0,0 +1,20 @@
+import 'package:redux/redux.dart';
+
+import 'package:blazehub/models/app.dart';
+import 'package:blazehub/models/auth.dart';
+
+class ProfileViewModel {
+  final AuthState authState;
+  final Store<AppState> _store;
+
+  ProfileViewModel(store, {this.authState}) : _store = store;
+
+  factory ProfileViewModel.create(Store<AppState> store) {
+    return ProfileViewModel(
+      store,
+      authState: store.state.authState,
+    );
+  }
+
+  getCoverPhoto() {}
+}
