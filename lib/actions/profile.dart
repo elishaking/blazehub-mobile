0aedd9a223129f702f0c5af779b6770f1cb7c440
King Elisha <ek.chibueze@gmail.com>
2020-03-26 10:46:19 +0100

    profile actions

---
+++ lib/actions/profile.dart
@@ -0,0 +1,13 @@
+import 'dart:typed_data';
+
+class SetProfilePicture {
+  final Uint8List payload;
+
+  SetProfilePicture(this.payload);
+}
+
+class SetCoverPhoto {
+  final Uint8List payload;
+
+  SetCoverPhoto(this.payload);
+}
