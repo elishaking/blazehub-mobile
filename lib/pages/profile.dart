bc3cd5a32760c72aefd2d137356745634e5155ac
King Elisha <ek.chibueze@gmail.com>
2020-03-26 09:45:28 +0100

    update

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -5,9 +5,21 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Profile'),
+      appBar: AppBar(
+        leading: Icon(Icons.person),
+        title: Text('John Doe'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                  // child: Image.memory(bytes),
+                  )
+            ],
+          )
+        ],
       ),
       bottomNavigationBar: Hero(
         tag: 'bottomNav',
