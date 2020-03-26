9f7811a1b25184acaf013369560c5e27f142636e
King Elisha <ek.chibueze@gmail.com>
2020-03-26 11:02:53 +0100

    updated profile state

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -1,21 +1,62 @@
 import 'dart:typed_data';
+import 'package:blazehub/pages/profile.dart';
 import 'package:flutter/foundation.dart';
+
+class Profile {
+  String name;
+  String username;
+  String website;
+  int birth;
+  String bio;
+  String location;
+
+  Profile({
+    @required this.name,
+    @required this.username,
+    @required this.website,
+    @required this.birth,
+    @required this.bio,
+    @required this.location,
+  });
+
+  Profile copyWith({
+    String name,
+    String username,
+    String website,
+    int birth,
+    String bio,
+    String location,
+  }) =>
+      Profile(
+        name: name ?? this.name,
+        username: username ?? this.username,
+        website: website ?? this.website,
+        birth: birth ?? this.birth,
+        bio: bio ?? this.bio,
+        location: location ?? this.location,
+      );
+}
 
 class ProfileState {
   Uint8List profilePicture;
   Uint8List coverPicture;
+  Profile profileInfo;
 
   ProfileState({
     @required this.profilePicture,
     @required this.coverPicture,
+    @required this.profileInfo,
   });
 
   ProfileState copyWith({
     Uint8List profilePicture,
+    Uint8List coverPicture,
+    Profile profileInfo,
   }) {
     return ProfileState(
       profilePicture: profilePicture ?? this.profilePicture,
       coverPicture: coverPicture ?? this.coverPicture,
+      profileInfo: profileInfo ?? this.profileInfo,
     );
   }
 }
