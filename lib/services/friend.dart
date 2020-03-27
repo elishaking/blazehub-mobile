8e6f335a9f3f60c960ea41a2914f34d8d0547f0e
King Elisha <ek.chibueze@gmail.com>
2020-03-27 19:17:14 +0100

    getFriends service

---
+++ lib/services/friend.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_database/firebase_database.dart';
+
+import 'package:blazehub/models/friend.dart';
+
+class FriendService {
+  final _dbRef = FirebaseDatabase.instance.reference();
+
+  Future<Map<String, Friend>> getFriends(String userID) async {
+    try {
+      final friendsSnapshot =
+          await _dbRef.child('friends').child(userID).once();
+
+      if (friendsSnapshot.value == null) return null;
+
+      return friendsSnapshot.value.map(
+          (friendKey, friend) => MapEntry(friendKey, Friend.fromJSON(friend)));
+    } catch (err) {
+      print(err);
+
+      return null;
+    }
+  }
+}
