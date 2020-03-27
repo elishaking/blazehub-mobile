5acd7f1f4c9a039a12cacd041e4adc51a4daccef
King Elisha <ek.chibueze@gmail.com>
2020-03-27 19:23:21 +0100

    getFriends view model

--- lib/services/friend.dart
+++ lib/services/friend.dart
@@ -21,3 +21,5 @@ class FriendService {
     }
   }
 }
+
+final friendService = FriendService();
--- lib/view_models/friend.dart
+++ lib/view_models/friend.dart
@@ -1,9 +1,21 @@
+import 'package:blazehub/actions/friend.dart';
+import 'package:blazehub/services/friend.dart';
 import 'package:redux/redux.dart';
 
 import 'package:blazehub/models/app.dart';
+import 'package:blazehub/models/friend.dart';
 
 class FriendViewModel {
   final Store<AppState> _store;
 
   FriendViewModel(this._store);
+
+  Future<bool> getFriends(String userID) async {
+    final friends = await friendService.getFriends(userID);
+
+    if (friends == null) return false;
+
+    _store.dispatch(SetFriends(friends));
+    return true;
+  }
 }
