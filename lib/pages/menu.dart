8005f4254f3cd5cca7bb2b26f41d7dda23d73b47
King Elisha <ek.chibueze@gmail.com>
2020-03-30 19:20:18 +0100

    integrated menu view model

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,5 +1,9 @@
 import 'package:blazehub/components/BottomNav.dart';
+import 'package:blazehub/models/app.dart';
+import 'package:blazehub/pages/add_friend.dart';
+import 'package:blazehub/view_models/menu.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 class Menu extends StatelessWidget {
   @override
@@ -9,20 +13,24 @@ class Menu extends StatelessWidget {
         centerTitle: true,
         title: Text('Menu'),
       ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.person_add),
-            title: Text("Add Friend"),
-            onTap: () {
-              // Navigator.of(context).push(MaterialPageRoute(builder: (context) =>
-              // AddFriend(model)));
-            },
-          ),
-          Divider()
-        ],
-      ),
+      body: StoreConnector<AppState, MenuViewModel>(
+          converter: (store) => MenuViewModel.create(store),
+          builder: (context, model) {
+            return ListView(
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.person_add),
+                  title: Text("Add Friend"),
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => AddFriend(model)));
+                  },
+                ),
+                Divider()
+              ],
+            );
+          }),
       bottomNavigationBar: Hero(
         tag: 'bottomNav',
         child: BottomNav(2),
