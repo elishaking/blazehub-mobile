6fa188bab50f02fde4b165d9338463af5c5a7df1
King Elisha <ek.chibueze@gmail.com>
2020-04-02 13:35:14 +0100

    show bookmarks

--- lib/pages/bookmarks.dart
+++ lib/pages/bookmarks.dart
@@ -1,25 +1,56 @@
+import 'package:blazehub/components/PostWidget.dart';
+import 'package:blazehub/components/Spinner.dart';
 import 'package:flutter/material.dart';
 import 'package:redux/redux.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
 import 'package:blazehub/models/app.dart';
 import 'package:blazehub/view_models/post.dart';
+import 'package:blazehub/models/posts.dart';
+
+bool retrievedBookmarks = false;
+Map<String, Post> bookmarkedPosts = Map();
 
 class Bookmarks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoreConnector(
+    return StoreConnector<AppState, PostViewModel>(
       converter: (Store<AppState> store) => PostViewModel(store),
       builder: (context, model) {
+        if (!retrievedBookmarks) {
+          model.getBookmarks(model.authState.user.id).then((posts) {
+            bookmarkedPosts = posts;
+          });
+          retrievedBookmarks = true;
+        }
+
         return Scaffold(
           appBar: AppBar(
             title: Text('Bookmarks'),
             centerTitle: true,
           ),
-          body: Center(
-            child: Text('Bookmarks'),
-          ),
+          body: _buildBookmarks(model),
         );
+      },
+    );
+  }
+
+  Widget _buildBookmarks(PostViewModel model) {
+    if (!retrievedBookmarks)
+      return Center(
+        child: Spinner(),
+      );
+
+    if (bookmarkedPosts == null)
+      return Center(
+        child: Text("No Bookmarked Posts"),
+      );
+
+    final postKeys = bookmarkedPosts.keys.toList();
+    return ListView.builder(
+      itemCount: bookmarkedPosts.length,
+      itemBuilder: (context, index) {
+        return PostWidget(bookmarkedPosts[postKeys[index]], model);
       },
     );
   }
