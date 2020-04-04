fbaf21c99df1b80d8e0f78338736e49ab013ac9c
King Elisha <ek.chibueze@gmail.com>
2020-04-04 17:23:26 +0100

    added newMessageAdded service

---
+++ lib/services/chat.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_database/firebase_database.dart';
+
+final now = DateTime.now().millisecondsSinceEpoch;
+
+class ChatService {
+  final _dbRef = FirebaseDatabase.instance.reference();
+
+  Stream<Event> newMessageAdded(String chatID) {
+    return _dbRef
+        .child('chats')
+        .child(chatID)
+        .orderByChild('date')
+        .startAt(now)
+        .onChildAdded;
+  }
+}
