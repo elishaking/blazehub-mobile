55beea3f9c0423116f9c4f851f42323a5c4d32ac
King Elisha <ek.chibueze@gmail.com>
2020-04-04 17:10:30 +0100

    updated

--- lib/models/chat.dart
+++ lib/models/chat.dart
@@ -1,12 +1,25 @@
+import 'package:blazehub/pages/chat.dart';
 import 'package:flutter/foundation.dart';
 
-class Chat {
-  final String message;
+class Message {
+  final String text;
   final int date;
+  final String userID;
+
+  Message({
+    @required this.text,
+    @required this.date,
+    @required this.userID,
+  });
+}
+
+class Chat {
+  final String chatID;
+  final Map<String, Message> messages;
 
   Chat({
-    @required this.message,
-    @required this.date,
+    @required this.chatID,
+    @required this.messages,
   });
 }
 
@@ -14,6 +27,26 @@ class ChatState {
   final Map<String, Chat> chats;
 
   ChatState({@required this.chats});
+
+  ChatState copyWithMessage(String chatID, Map<String, Message> messages) {
+    final newChats = Map<String, Chat>();
+    if (chats != null) newChats.addAll(chats);
+
+    if (newChats[chatID] == null)
+      newChats.putIfAbsent(
+        chatID,
+        () => Chat(
+          chatID: chatID,
+          messages: messages,
+        ),
+      );
+    else
+      messages.forEach((messageKey, message) {
+        newChats[chatID].messages[messageKey] = message;
+      });
+
+    return ChatState(chats: newChats);
+  }
 
   ChatState copyWith({Map chats}) {
     final newChats = Map<String, Chat>();
