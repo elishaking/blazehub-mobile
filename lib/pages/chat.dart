aacbcc72b9a0c369588e1dadc30677310b9e6474
King Elisha <ek.chibueze@gmail.com>
2020-04-04 17:24:10 +0100

    chart ==> chat

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,7 +1,7 @@
 import 'package:blazehub/components/BottomNav.dart';
 import 'package:blazehub/models/app.dart';
 import 'package:blazehub/pages/menu.dart';
-import 'package:blazehub/view_models/chart.dart';
+import 'package:blazehub/view_models/chat.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
