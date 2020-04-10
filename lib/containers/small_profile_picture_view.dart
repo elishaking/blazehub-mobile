bc9c40196174fa91ba5cbe80f406d04bc8c0d3a2
King Elisha <ek.chibueze@gmail.com>
2020-04-10 17:40:00 +0100

    SmallProfilePictureView added

---
+++ lib/containers/small_profile_picture_view.dart
@@ -0,0 +1,40 @@
+import 'dart:typed_data';
+
+import 'package:blazehub/components/Spinner.dart';
+import 'package:blazehub/models/app.dart';
+import 'package:blazehub/view_models/friend.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+
+class SmallProfilePictureView extends StatelessWidget {
+  final Uint8List smallProfilePicture;
+  final String heroTag;
+
+  SmallProfilePictureView(this.smallProfilePicture, this.heroTag);
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, FriendViewModel>(
+      converter: (store) => FriendViewModel(store),
+      builder: (context, model) {
+        return Scaffold(
+          appBar: AppBar(),
+          body: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              Hero(
+                tag: heroTag,
+                child: Image.memory(
+                  smallProfilePicture,
+                  width: double.maxFinite,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Spinner(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
